116cd388976e2363c2e1fddcda3743727913e2f3
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 14:34:40 +0900

    [refactoring] encapsulation variables

--- game.py
+++ game.py
@@ -8,7 +8,8 @@ class Game:
 
     def guess(self, guessNumber):
         self.assert_illegal_value(guessNumber)
-        return GameResult(True, 3, 0)
+        if self.question == guessNumber:
+            return GameResult(True, 3, 0)
 
     def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
