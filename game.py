e3bceef11d9ebb5b41e1ebdd10cebba85b809d93
Eunji.Heo <eunja.heo@samsung.com>
2024-06-13 14:04:14 +0900

    [refactoring] divide method

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 class Game:
     def guess(self, guessNumber):
+        self.assert_illegal_value(guessNumber)
+
+    def assert_illegal_value(self, guessNumber):
         if guessNumber is None:
             raise TypeError()
         if len(guessNumber) != 3:
@@ -8,7 +11,9 @@ class Game:
             int(guessNumber)
         except ValueError as e:
             raise TypeError()
-
-        if len(guessNumber) != len(set(guessNumber)):
+        if self.isDuplicatedNumber(guessNumber):
             raise TypeError()
+
+    def isDuplicatedNumber(self, guessNumber):
+        return len(guessNumber) != len(set(guessNumber))
 
